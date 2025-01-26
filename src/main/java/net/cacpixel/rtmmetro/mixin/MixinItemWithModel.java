5ca846f5722af35fe5313dfae0e3ad2be4e44526
CacPixel <caochao233@qq.com>
2025-01-27 00:41:44 +0800

    fix: ngt拉的一坨香的：若已经访问过模型选择gui，下次打开带有铁轨的创造模式界面会莫名其妙修改手持物品的ModelState

---
+++ src/main/java/net/cacpixel/rtmmetro/mixin/MixinItemWithModel.java
@@ -0,0 +1,56 @@
+package net.cacpixel.rtmmetro.mixin;
+
+import jp.ngt.ngtlib.item.ItemCustom;
+import jp.ngt.ngtlib.network.PacketNBT;
+import jp.ngt.rtm.item.ItemWithModel;
+import jp.ngt.rtm.modelpack.modelset.ResourceSet;
+import jp.ngt.rtm.modelpack.state.ResourceState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ItemWithModel.class, remap = false)
+public class MixinItemWithModel<T extends ResourceSet> extends ItemCustom
+{
+    @Shadow
+    private ItemStack selectedItem;
+
+    @Shadow
+    private EntityPlayer selectedPlayer;
+
+    @Inject(method = "setModelState", at = @At(value = "INVOKE", target =
+            "Ljp/ngt/ngtlib/network/PacketNBT;sendToServer(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/item/ItemStack;)V"),
+            cancellable = true)
+    public void setModelState(ItemStack itemStack, ResourceState<T> state, CallbackInfo ci)
+    {
+        ci.cancel();
+    }
+
+    @Inject(method = "closeGui", at = @At("HEAD"), cancellable = true)
+    public void closeGui(ResourceState par1, CallbackInfoReturnable<Boolean> cir)
+    {
+        this.setModelStateAndSendPacket(this.selectedItem, par1);
+        cir.setReturnValue(true);
+    }
+
+    @Unique
+    public void setModelStateAndSendPacket(ItemStack itemStack, ResourceState<T> state)
+    {
+        if (!itemStack.hasTagCompound())
+        {
+            itemStack.setTagCompound(new NBTTagCompound());
+        }
+        itemStack.getTagCompound().setTag("State", state.writeToNBT());
+        if (this.selectedPlayer != null)
+        {
+            PacketNBT.sendToServer(this.selectedPlayer, this.selectedItem);
+        }
+    }
+}
