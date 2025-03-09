30e9427e2ae05447af93595b3590d02faff1e69d
CacPixel <caochao233@qq.com>
2025-03-09 22:08:12 +0800

    feat: fast modelpack construct test 1

---
+++ src/main/java/net/cacpixel/rtmmetro/mixin/early/MixinModelPackConstructThread.java
@@ -0,0 +1,88 @@
+package net.cacpixel.rtmmetro.mixin.early;
+
+import jp.ngt.ngtlib.io.NGTLog;
+import jp.ngt.rtm.modelpack.ModelPackManager;
+import jp.ngt.rtm.modelpack.init.ModelPackConstructThread;
+import jp.ngt.rtm.modelpack.init.ModelPackLoadThread;
+import jp.ngt.rtm.modelpack.init.ProgressStateHolder;
+import jp.ngt.rtm.modelpack.modelset.ResourceSet;
+import net.minecraftforge.fml.relauncher.Side;
+import org.spongepowered.asm.mixin.*;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Mixin(value = ModelPackConstructThread.class, remap = false)
+public class MixinModelPackConstructThread extends Thread
+{
+    @Final
+    @Shadow
+    private final Side threadSide = null;
+    @Final
+    @Shadow
+    private final ModelPackLoadThread parent = null;
+    @Shadow
+    private boolean loading;
+    @Unique
+    private AtomicInteger rtmmetro$index = new AtomicInteger(0);
+    @Shadow
+    private boolean barStateChanged;
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void runThread() throws InterruptedException
+    {
+        while (this.loading)
+        {
+            while (ModelPackManager.INSTANCE.unconstructSets.size() > this.rtmmetro$index.get())
+            {
+                ResourceSet set = (ResourceSet) ModelPackManager.INSTANCE.unconstructSets.get(this.rtmmetro$index.get());
+                if (this.threadSide == Side.SERVER)
+                {
+                    set.constructOnServer();
+                }
+                else
+                {
+                    set.constructOnClient();
+                    set.finishConstruct();
+                }
+
+                NGTLog.debug("Construct Model : %s (%d / %d)",
+                        set.getConfig().getName(), this.rtmmetro$index.get() + 1, ModelPackManager.INSTANCE.unconstructSets.size());
+                rtmmetro$index.getAndIncrement();
+                if (this.parent.loadFinished)
+                {
+                    if (!this.barStateChanged)
+                    {
+                        this.parent.setBarValue(0, ProgressStateHolder.ProgressState.CONSTRUCTING_MODEL);
+                        int size = ModelPackManager.INSTANCE.unconstructSets.size();
+                        this.parent.setBarMaxValue(1, size, "");
+                        this.barStateChanged = true;
+                    }
+
+                    this.parent.setBarValue(1, this.rtmmetro$index.get(), set.getConfig().getName());
+                }
+            }
+
+            sleep(500L);
+        }
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public boolean setFinish() {
+        if (ModelPackManager.INSTANCE.unconstructSets.size() == this.rtmmetro$index.get()) {
+            ModelPackManager.INSTANCE.unconstructSets.clear();
+            ModelPackManager.INSTANCE.clearCache();
+            this.loading = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/net/cacpixel/rtmmetro/modelpack/init/FastModelPackConstructor.java
@@ -0,0 +1,11 @@
+package net.cacpixel.rtmmetro.modelpack.init;
+
+public class FastModelPackConstructor
+{
+    public static final FastModelPackConstructor INSTANCE = new FastModelPackConstructor();
+
+    public FastModelPackConstructor()
+    {
+
+    }
+}
